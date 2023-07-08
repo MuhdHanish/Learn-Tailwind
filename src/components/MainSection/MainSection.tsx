725ba6b2367faed851d6cf657364668ddb2e7cf4
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-08 17:29:27 +0530

    created a reusbale button

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,13 +1,19 @@
 const MainSection = () => {
   return (
     <div className="flex gap-10 justify-center md:justify-end">
-      <a href="#" className="text-primary-600">
+      <a
+        href="#"
+        className="btn text-primary-600 md:border border-primary-600"
+      >
         Log in
       </a>
-      <a href="#" className="text-primary-600">
+      <a
+        href="#"
+        className="btn text-primary-600 md:border border-primary-600"
+      >
         Sign up
       </a>
-      </div>
+    </div>
   );
 }
 
