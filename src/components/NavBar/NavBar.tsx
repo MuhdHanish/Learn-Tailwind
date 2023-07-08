cb0b8bbc8dd48e3bd8f78b004df610821677d12f
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-08 21:17:06 +0530

    responsive navbar part -2 added a usestate to make a action to a button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,28 @@
+import {useState} from 'react';
 import { AiOutlineHome } from "react-icons/ai";
 import { FcAbout } from "react-icons/fc";
-import { BiSolidContact } from "react-icons/bi";
+import { BiSolidContact, BiMenuAltLeft } from "react-icons/bi";
 
 const NavBar = () => {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <nav className="text-right">
-      <div>
+      <div className="flex justify-between items-center">
         <h1 className="font-bold uppercase p-4 border-b border-gray-100">
           <a href="/" className="hover:text-gray-700">
             Food Ninja
           </a>
         </h1>
+        <div className="px-4 cursor-pointer md:hidden ">
+          <BiMenuAltLeft style={{ fontSize: "20px" }} onClick={()=>setIsOpen(!isOpen)} />
+        </div>
       </div>
-      <ul className="text-sm mt-6 ">
+      <ul
+        id="menu"
+        className={isOpen ? "text-sm mt-6 block" : "text-sm mt-6 hidden md:block"}
+      >
         <li className="text-gray-700 font-bold py-1">
           <a
             href="#"
