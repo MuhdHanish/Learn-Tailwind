f5ed1807e04825e49d5889cac2b72df22d88f5ed
MuhdHanish <muhammedhanish11@gmail.com>
2023-07-08 12:29:43 +0530

    created a time badge and it positioned at the top left corner using the relative for parent and absolute for child

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,22 @@
-
-
 const Card = () => {
   return (
     <>
-      <div className="bg-white rounded overflow-hidden shadow-2xl ">
-        <img src="img/stew.jpg" alt="stew" className="w-full h-40 sm:h-60 object-cover" />
+      <div className="bg-white rounded overflow-hidden shadow-2xl relative">
+        <img
+          src="img/stew.jpg"
+          alt="stew"
+          className="w-full h-40 sm:h-60 object-cover"
+        />
         <div className="m-4">
           <span className="font-bold">5 Bean Chilli Stew</span>
           <span className="block text-gray-500 text-sm">Recipe by Mario</span>
         </div>
+        <div className="bg-green-400 text-gray-600 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
+          <span>25 min</span>
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Card
+export default Card;
--- src/components/LatestRecipe/LatestRecipe.tsx
+++ src/components/LatestRecipe/LatestRecipe.tsx
@@ -4,9 +4,11 @@ import Card from "../Card/Card";
 const LatestRecipe = () => {
   return (
     <>
-      <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">Latest Recipes</h4>
+      <h4 className="font-bold mt-12 pb-2 border-b border-gray-200">
+        Latest Recipes
+      </h4>
       <div className="mt-8">
-        <Card/>
+        <Card />
       </div>
     </>
   );
